12aa4bbb30e83227e6ea47a2c6ac349819740d7f
Karim Ho <karim.ho@avepoint.com>
2024-10-02 13:19:38 +0700

    update code

--- Models/Test.cs
+++ Models/Test.cs
@@ -1,1 +1,13 @@
-//dassd
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesLab.Models
+{
+    public class Test
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
